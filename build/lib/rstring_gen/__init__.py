583f37b70792adcfc8db3cdddc507e5fefc6510f
whatiskeptiname <susangmree@gmail.com>
2022-04-20 17:15:26 +0545

    removed unnecessary files

--- build/lib/rstring_gen/__init__.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-
-def gen_str(length) -> str:
-    """
-    Generate a random string of length `length`
-    """
-    return "".join(random.choice(string.ascii_letters) for _ in range(length))
